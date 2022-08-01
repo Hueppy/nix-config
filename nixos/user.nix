64c3cce760ebf643b3c4aa6cdf1e8dbc9e721b99
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2022-08-01 17:40:05 +0200

    introduce x11

--- nixos/user.nix
+++ nixos/user.nix
@@ -19,5 +19,43 @@
     programs = {
       zsh.enable = true;
     };
+
+    services = {
+      polybar = {
+        config = {
+	  "bar/main" = {
+            modules-left = [
+	      "bspwm"
+	    ];
+	  };
+        };
+        enable = true;
+      }
+
+      sxhkd = {
+        enable = true;
+	keybindings = {
+          "alt + Return" = "alacritty"
+        }
+      };
+
+      xserver.windowManager.bspwm = {
+        enable = true;
+	monitors = {
+          LVDS-1 = [
+	    "一"
+            "二"
+	    "三"
+	    "四"
+	    "五"
+	    "六"
+	    "七"
+	    "八"
+	    "九"
+	    "十"
+	  ];
+        };
+      };
+    }
   };
 }
